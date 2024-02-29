a16d96b7d0e9748a9b226b22af0884d112aedc63
AsGamer1 <asilva.ys@gmail.com>
2024-02-29 20:11:05 +0100

    Comentarios en JS y arreglos menores

--- js/monedas.js
+++ js/monedas.js
@@ -1,64 +1,67 @@
+// Muestra el último hijo del elemento que se pase como parámetro
+// Este elemento que se pasa por parámetro es el contenedor de las estrellas
 function showValorar(elemento) {
   let valorarButton = elemento.lastElementChild
   valorarButton.style.display = "inline-block"
 }
-
+// Ocultar el elemento anteriormente descrito
 function hideValorar(elemento) {
   let valorarButton = elemento.lastElementChild
   valorarButton.style.display = "none"
 }
-
+// La función que genera las estrellas de valoración de cada elemento numismático
+// Tiene como parámetros un elemento, el botón de valorar, y un parámetro opcional, que si está presente asigna ese valor en vez del input del usuario
 function valorar(elemento, valorInherito) {
-  let valor
-  if (!valorInherito) {
-    valor = elemento.previousElementSibling.value
+  let valor // declarar valor
+  if (!valorInherito) { // si no se pasa el parámetro, se coge el valor del input
+    valor = elemento.previousElementSibling.value // anterior hermano del botón = input numérico
   } else {
-    valor = valorInherito
+    valor = valorInherito // sino, existe el parámetro y se pasa este en su lugar
   }
-  let valorFlag = valor
-  let noFlag = 10 - valor
-  let div = elemento.parentElement.parentElement
-  let article = elemento.parentElement.parentElement.parentElement.parentElement
-  if (valor >= 0 && valor <= 10) {
-    if (valor % 2 != 0) {
-      div.innerHTML = ""
-      while (valorFlag > 1) {
-        div.innerHTML += `<img src="media/svg/Star.svg" alt="★">`
-        valorFlag = valorFlag - 2
+  let valorFlag = valor // Una copia del valor, para asignar las estrellas
+  let noFlag = 10 - valor // Para asignar las estrellas vacías
+  let div = elemento.parentElement.parentElement // El padre del padre del botón, es decir, el div que contiene las estrellas
+  let article = elemento.parentElement.parentElement.parentElement.parentElement // El padre del padre del padre del padre del botón, es decir, el artículo que contiene el ID de la moneda a valorar
+  if (valor >= 0 && valor <= 10) { // Si el valor está entre 0 y 10 se ejecuta la función
+    div.innerHTML = ""
+    if (valor % 2 != 0) { // Si el valor es impar, se genera una media estrella
+      while (valorFlag > 1) { // Al ser impar, el valor más bajo será 1
+        div.innerHTML += `<img src="media/svg/Star.svg" alt="★">` // Se generan estrellas enteras
+        valorFlag = valorFlag - 2 // Se resta 2 al valor, pues cada estrella está compuesta de 2 medias partes
       }
-      div.innerHTML += `<img src="media/svg/HalfStar.svg" alt="★">`
-      while (noFlag > 1) {
-        div.innerHTML += `<img src="media/svg/NoStar.svg" alt="★">`
-        noFlag = noFlag - 2
-      }
-      div.innerHTML += `<div class="valorar">
-                          <input type="text" inputmode="numeric" onkeydown="return /[0-9]|Arrow|Backspace|Delete/.test(event.key)">
-                          <button onclick="valorar(this)">Valorar</button>
-                        </div>`
+      div.innerHTML += `<img src="media/svg/HalfStar.svg" alt="★">` // Se genera una media estrella
     } else {
-      div.innerHTML = ""
-      while (valorFlag > 0) {
+      while (valorFlag > 0) { // Al ser par, el valor más bajo será 0
         div.innerHTML += `<img src="media/svg/Star.svg" alt="★">`
         valorFlag = valorFlag - 2
+        // No se genera la media estrella
       }
-      while (noFlag > 1) {
-        div.innerHTML += `<img src="media/svg/NoStar.svg" alt="★">`
-        noFlag = noFlag - 2
-      }
-      div.innerHTML += `<div class="valorar">
-                          <input type="text" inputmode="numeric" onkeydown="return /[0-9]|Arrow|Backspace|Delete/.test(event.key)">
+    }
+    while (noFlag > 1) { // Las estrellas restantes, independientemente de si es par o inpar el valor se compara con 1
+      div.innerHTML += `<img src="media/svg/NoStar.svg" alt="★">` // Se genera la estrella vacía
+      noFlag = noFlag - 2 // Se resta 2 al valor
+    }
+    // Una vez generadas las estrellas, se añade debajo el mismo div con el botón de valorar, para poder re-valorar en cualquier momento.
+    div.innerHTML += `<div class="valorar">
+                          <input type="text" placeholder="Valoración del 0 al 10" inputmode="numeric" onkeydown="return /[0-9]|Arrow|Backspace|Delete/.test(event.key)">
                           <button onclick="valorar(this)">Valorar</button>
                         </div>`
-    }
+    // Se guarda en localStorage un item que tiene de nombre el id del artículo y como valor las estrellas que tiene, para que cada vez que se cargue el documento se asignen de nuevo.
     localStorage.setItem(article.id, valor)
     
-  } else {
+  } else { // Sino está el valor entre 0 y 10, se pone de color rojo el input
     elemento.previousElementSibling.style.backgroundColor = "#F68E84"
   }
 }
 
+// Cada vez que se carga el script, se genera un array con los contenedores de las estrellas y otro con los artículos que hay.
 let estrellas = document.getElementsByClassName("valorar")
 let valorados = document.getElementsByTagName("article")
+// Para cada contenedor de estrellas que haya, se realiza lo siguiente:
+// Se pasa como parámetro a la función de valorar el último hijo del contenedor de valorar, es decir, el botón. Esto tiene el mismo efecto que haber pulsado el botón y pasarse a sí mismo como parámetro.
+// Además, se pasa un valorInherito, el valor del item guardado al ser ejecutada la función por el usuario.
 for (let i=0; i < estrellas.length; i++) {
   valorar(estrellas[i].lastElementChild,localStorage.getItem(valorados[i].id))
 }
+// NOTA: Al cargarse la página se generan las estrellas siempre, por lo que en el HTML son elementos que no existen hasta que no se carge.
+// NOTA: Al cargarse por primera vez una página, ningún valor estará guardado en localStorage, por lo que se pasa como valor "null", significando que la función cogerá el valor del input, que por defecto está vacío. El código lo interpreta como que es 0, significando que todas las estrellas serán vacías.
